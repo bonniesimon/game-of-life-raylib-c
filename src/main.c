093320cd290a88089fd9d3a9197899626e037ccb
Bonnie Simon <bonnie.developer@gmail.com>
2023-12-25 12:55:46 +0530

    Add update_state logic. Note: Blinker not working

--- src/main.c
+++ src/main.c
@@ -14,6 +14,9 @@ typedef struct {
   int cols;
 } Board;
 
+int neighbors[][2] = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0},
+                      {1, 0},   {-1, 1}, {0, 1},  {1, 1}};
+
 bool **create_grid(int rows, int cols) {
   bool **grid = (bool **)malloc(rows * sizeof(bool *));
 
@@ -40,9 +43,14 @@ bool **create_grid(int rows, int cols) {
 void fill_grid(Board *board) {
   for (int r = 0; r < board->rows; r++) {
     for (int c = 0; c < board->cols; c++) {
-      board->grid[r][c] = rand() % 2 == 0 ? false : true;
+      board->grid[r][c] = GetRandomValue(0, 9) > 8 ? true : false;
     }
   }
+
+  // board->grid[1][1] = true;
+  // board->grid[2][1] = true;
+  // board->grid[3][1] = true;
+  // board->grid[2][2] = true;
 }
 
 void print_grid(Board *board) {
@@ -54,13 +62,84 @@ void print_grid(Board *board) {
   }
 }
 
-void update_state(){};
+int calculate_neighbor_sum(Board *board, int row, int col) {
+  int sum = 0;
+
+  for (int x = 0; x < 8; x++) {
+    int new_row = row + neighbors[x][0];
+    int new_col = col + neighbors[x][1];
+
+    if (new_col < 0 || new_row < 0 || new_col > board->cols - 1 ||
+        new_row > board->rows - 1) {
+      continue;
+    }
+
+    sum = sum + board->grid[new_row][new_col];
+  }
+
+  if (row == 1 && col == 1) {
+    printf("sum = %d\n", sum);
+  }
+
+  return sum;
+}
+
+bool is_underpopulated(Board *board, int row, int col) {
+  int sum = calculate_neighbor_sum(board, row, col);
+
+  return sum < 2;
+}
+
+bool is_new_generation_possible(Board *board, int row, int col) {
+  int sum = calculate_neighbor_sum(board, row, col);
+
+  return sum == 2 || sum == 3;
+}
+
+bool is_overpopulated(Board *board, int row, int col) {
+  int sum = calculate_neighbor_sum(board, row, col);
+
+  return sum > 3;
+}
+
+bool is_reproduction(Board *board, int row, int col) {
+  int sum = calculate_neighbor_sum(board, row, col);
+
+  return sum == 3;
+}
+
+void update_state(Board *board) {
+  printf("------------------------------\n\n");
+  for (int r = 0; r < board->rows; r++) {
+    for (int c = 0; c < board->cols; c++) {
+      printf("%d, %d: %d\n", r, c, board->grid[r][c]);
+      if (board->grid[r][c] == false) {
+        if (is_reproduction(board, r, c)) {
+          printf("set_to_true => %d, %d = %d\n", r, c, board->grid[r][c]);
+          board->grid[r][c] = true;
+        }
+      }
+
+      if (board->grid[r][c] == true) {
+        if (is_underpopulated(board, r, c) || is_overpopulated(board, r, c)) {
+          printf("set_to_false => %d, %d = %d\n", r, c, board->grid[r][c]);
+          board->grid[r][c] = false;
+        }
+
+        if (is_new_generation_possible(board, r, c)) {
+          board->grid[r][c] = true;
+        }
+      }
+    }
+  }
+};
+
 void draw_state(Board *board) {
   for (int r = 0; r < board->rows; r++) {
     for (int c = 0; c < board->cols; c++) {
       if (board->grid[r][c] == true) {
         int scale = 10;
-        DrawRectangle(r * scale, c * scale, scale, scale, YELLOW);
+        DrawRectangle(r * scale, c * scale, scale, scale, WHITE);
       }
     }
   }
@@ -68,10 +147,12 @@ void draw_state(Board *board) {
 
 int main() {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Conway's Game of Life");
-  SetTargetFPS(60);
+  SetTargetFPS(1);
 
   int grid_width = SCREEN_WIDTH / 10;
   int grid_height = SCREEN_HEIGHT / 10;
+  // int grid_width = 10;
+  // int grid_height = 10;
 
   Board board = {0};
 
@@ -79,13 +160,14 @@ int main() {
   board.rows = grid_height;
   board.cols = grid_width;
   fill_grid(&board);
-  print_grid(&board);
+  // print_grid(&board);
+  draw_state(&board);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     {
       ClearBackground(DARKGRAY);
-      update_state();
+      update_state(&board);
       draw_state(&board);
     }
     EndDrawing();
